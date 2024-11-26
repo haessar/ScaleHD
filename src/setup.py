20485f46c170288a0435d72c93ba264787c6430c
haessar <wahhill@gmail.com>
2024-11-26 23:50:51 +0000

    increment semver

--- src/setup.py
+++ src/setup.py
@@ -13,7 +13,7 @@ setup(
     name='ScaleHD',
 
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0',
+    version='1.1.0',
 
     description='Automated DNA micro-satellite genotyping.',
     long_description=long_description,
